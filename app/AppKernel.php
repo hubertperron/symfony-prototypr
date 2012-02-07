1172ebfd6d9e24711a5dc7a2c4520af50ac559fe
Hubert Perron <hubert.perron@gmail.com>
2012-02-06 21:03:07 -0500

    Adding SystemBundle as a deps

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+
+            new Prototypr\SystemBundle\PrototyprSystemBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
